cd26c1af3aeb9c7da02148cef5cd8ab4f441f421
Sander Feenstra <sanderfeenstra97@hotmail.com>
2022-05-11 17:28:15 +0200

    Adds logic for authenticating users

--- cms/includes/UserDatabase.class.php
+++ cms/includes/UserDatabase.class.php
@@ -5,20 +5,22 @@
 
 	class UserDatabase extends SQLite3
 	{
-		function __construct()
+		private static $TOKEN_DURATION = 3600;
+
+		public function __construct()
 		{
 			parent::__construct("cms/data/user.db");
 			
-			$this->initialise();
+			self::initialise();
 		}
 
-		function initialise()
+		private function initialise()
 		{
-			$createStatement = $this->prepare("CREATE TABLE IF NOT EXISTS users (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, UserName varchar(255) NOT NULL UNIQUE, PasswordHash char(128))");
-			$createStatement->execute();
-			$createStatement->close();
+			$createUsersStatement = self::prepare("CREATE TABLE IF NOT EXISTS users (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, UserName varchar(255) NOT NULL UNIQUE, PasswordHash char(128))");
+			$createUsersStatement->execute();
+			$createUsersStatement->close();
 
-			$selectStatement = $this->prepare("SELECT UserName FROM users");
+			$selectStatement = self::prepare("SELECT UserName FROM users");
 			$selectResult = $selectStatement->execute();
 
 			if (!$username = $selectResult->fetchArray()) {
@@ -30,19 +32,63 @@
 				fwrite($file, $password);
 				fclose($file);
 
-				$insertStatement = $this->prepare("INSERT INTO users (UserName, PasswordHash) VALUES ('admin', '" . $hash . "')");
+				$insertStatement = self::prepare("INSERT INTO users (UserName, PasswordHash) VALUES ('admin', :hash)");
+				$insertStatement->bindValue(':hash', $hash, SQLITE3_TEXT);
 				$insertStatement->execute();
-			} else if (file_exists("cms/password/pass.txt") && !file_exists("cms/password/newpass.txt")) {
+				$insertStatement->close();
+			} else if (file_exists("cms/password/pass.txt")) {
 				$file = fopen("cms/password/pass.txt", "r");
 				$password = fgets($file);
 
 				$hash = hash('sha512', $password);
 				
-				$updateStatement = $this->prepare("UPDATE users SET PasswordHash = '" . $hash . "' WHERE UserName = 'admin';");
+				$updateStatement = self::prepare("UPDATE users SET PasswordHash = :hash WHERE UserName = 'admin';");
+				$updateStatement->bindValue(":hash", $hash, SQLITE3_TEXT);
 				$updateStatement->execute();
+				$updateStatement->close();
 
 				fclose($file);
 				unlink("cms/password/pass.txt");
+			}
+
+			$selectStatement->close();
+
+			$createSessionStatement = self::prepare("CREATE TABLE IF NOT EXISTS sessions (userId INTEGER, token char(128), expires INTEGER)");
+			$createSessionStatement->execute();
+			$createSessionStatement->close();
+		}
+
+		public function authenticate(string $username, string $password)
+		{
+			$selectStatement = self::prepare("SELECT Id, PasswordHash FROM users WHERE UserName=:username LIMIT 1");
+			$selectStatement->bindValue(":username", $username);
+			$selectResult = $selectStatement->execute();
+
+			if (!$res = $selectResult.fetchArray()) {
+				return false;
+			} else {
+				$inputHash = hash('sha512', $password);
+
+				if ($inputHash == $res[1]) {
+					$rand = rand();
+					$token = hash('sha512', $rand);
+
+					$deleteStatement = self::prepare("DELETE FROM sesions WHERE userId=:userId");
+					$deleteStatement->bindValue(":userId", $res[0], SQLITE3_INT);
+					$deleteStatement->execute();
+					$deleteStatement->close();
+
+					$insertStatement = self::prepare("INSERT INTO sessions (userId, token, expires) VALUES (:userId, :token, :expires)");
+					$insertStatement->bindValue(":userId", $res[0], SQLITE3_INT);
+					$insertStatement->bindValue(":token", $token, SQLITE3_TEXT);
+					$insertStatement->bindValue(":expires", time() + self::$TOKENDURATION, SQLITE3_INT);
+					$insertStatement->execute();
+					$insertStatement->close();
+
+					return $token;
+				} else {
+					return false;
+				}
 			}
 		}
 	}
